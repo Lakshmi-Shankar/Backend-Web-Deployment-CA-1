8b31f6d2117376e92794a05532ebc4dbdab5112d
Lakshmi-Shankar <lakshmi7708671565@gmail.com>
2025-02-03 14:37:31 +0530

    Completed

---
+++ server.js
@@ -0,0 +1,29 @@
+const { error } = require("console");
+const express = require("express");
+const { json } = require("stream/consumers");
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+
+app.post("/Signup", (req, res)=>{
+    const {Username, Email, Password, DateofBirth} = req.body;
+
+    if(!Username){
+        return res.status(400).json({errorMessage : "Username cannot be empty"});
+    }
+    if(!Email){
+        return res.status(400).json({errorMessage : "Email cannot be empty"});
+    }
+    if (Password.length<8 || Password.length>16){
+        return res.status(400).json({errorMessage : "Password length should be greater than 8 or less than or equal to 16"});
+    }
+
+    res.json({
+        successMessage : "SignUp Successfull !"
+    })
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
